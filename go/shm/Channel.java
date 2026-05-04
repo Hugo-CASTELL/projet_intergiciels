1955f473c5dd42d46ffcd6173b8e66e449b012c9
Alexis-Wamster <wamsteralexis@gmail.com>
2026-05-04 17:11:14 +0200

    Observer

--- go/shm/Channel.java
+++ go/shm/Channel.java
@@ -7,28 +7,50 @@ import go.Direction;
 import go.Observer;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue; 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger; 
 
 public class Channel<T> implements go.Channel<T> {
 
     private String name;
     private final BlockingQueue<T> queue;
     private final BlockingQueue<Integer> queueIn;
+    private final List<Observer> inObservers;
+    private final List<Observer> outObservers;
+    private AtomicInteger inCounter;
+    private AtomicInteger outCounter;
 
     private static final int QUEUE_SIZE = 1000;
 
     public Channel(String name) {
         this.queue = new ArrayBlockingQueue<>(QUEUE_SIZE);
         this.queueIn = new ArrayBlockingQueue<>(QUEUE_SIZE);
+        this.inObservers = new ArrayList<>(QUEUE_SIZE);
+        this.outObservers = new ArrayList<>(QUEUE_SIZE);
+        this.inCounter = new AtomicInteger(0);
+        this.inCounter = new AtomicInteger(0);
+
         this.name = name;
     }
     
     public void out(T v) {
         try {
+            // indiquer la presence d'un redacteur
+            this.outCounter.incrementAndGet();
+            
+            // notifier les observers
+            while (this.outObservers.size() > 0){
+                this.outObservers.get(0).update();
+                this.outObservers.remove(0);
+            }
+
             // si aucun lecteur, bloquer jusqu'au prochain in
             this.queueIn.take();
+
             // ajouter la donner dans la file
             this.queue.put(v);
+            this.outCounter.decrementAndGet();
+
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -37,9 +59,23 @@ public class Channel<T> implements go.Channel<T> {
     public T in() {
         try {
             // indiquer la presence d'un lecteur
+            this.inCounter.incrementAndGet();
+
+            // notifier les observers
+            while (this.inObservers.size() > 0){
+                this.inObservers.get(0).update();
+                this.inObservers.remove(0);
+            }
+
+            // indiquer la presence d'un lecteur a la fonction out
             this.queueIn.put(1);
+
             // si la queue est vide bloquer jusqu'au prochain out
-            return this.queue.take();
+            T result = this.queue.take();
+
+            // envoyer le resultat
+            this.outCounter.decrementAndGet();
+            return result;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -50,7 +86,22 @@ public class Channel<T> implements go.Channel<T> {
     }
 
     public void observe(Direction dir, Observer observer) {
-        // TODO
+        if(dir == Direction.In){
+            if (this.inCounter.get() > 0){
+                observer.update();
+            }
+            else{
+                this.inObservers.add(observer);
+            }
+            
+        } else if(dir == Direction.Out){
+            if (this.inCounter.get() > 0){
+                observer.update();
+            }
+            else{
+                this.outObservers.add(observer);
+            }
+        }
     }
         
 }
--- go/shm/Selector.java
+++ go/shm/Selector.java
@@ -1,18 +1,36 @@
 package go.shm;
 
 import go.Direction;
+import go.Observer;
 import go.Channel;
 import java.util.Map;
 import java.util.Set;
 
 public class Selector implements go.Selector {
 
+    private final Map<Channel, Direction> chanelsMap;
+
     public Selector(Map<Channel, Direction> channels) {
-        // TODO
+        this.chanelsMap = channels;
     }
 
     public Channel select() {
         // TODO
+
+        // je peut faire un in si un channel est bloqué en out
+
+        // je peut faire un out si un channel est bloqué en in
+
+
+        for (var entry : this.chanelsMap.entrySet()){
+            Channel channel = entry.getKey();
+            Direction direction = entry.getValue();
+            
+            Observer observer = new Observer();
+            channel.observe(Direction.inverse(direction), observer);
+        }
+
+
         return null;
     }
 
