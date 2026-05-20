7ce5f42249d6819a09ae3259ef1c4be220f5a471
Alexis-Wamster <wamsteralexis@gmail.com>
2026-05-20 14:50:43 +0200

    socket start

--- go/Channel.java
+++ go/Channel.java
@@ -1,7 +1,9 @@
 package go;
 
+import java.rmi.Remote;
+
 /** Un canal de communication synchrone, qui permet d'envoyer et recevoir des messages de type T. */
-public interface Channel<T> extends java.io.Serializable {
+public interface Channel<T> extends java.io.Serializable, Remote {
 
     /** Envoi synchrone d'un message.
      * Bloque tant que le message ne peut pas être envoyé. */
